88585db43b063db619d0be7df6b12c692fd29f51
ambersr <amber.schalker@hva.nl>
2026-02-11 13:41:24 +0100

    ref: added comments to button component #8

--- apps/web/lib/components/atoms/Button.tsx
+++ apps/web/lib/components/atoms/Button.tsx
@@ -3,25 +3,31 @@
 import React from "react";
 import { PlusCircleIcon, UserPlusIcon, SignInIcon } from "@phosphor-icons/react";
 
+// Object waarin de iconen gekoppeld worden aan een key (voor dynamisch kiezen van een icoon)
 const icons = { 
   plus: PlusCircleIcon, 
   add: UserPlusIcon, 
   login: SignInIcon 
 };
 
+// Type definitie voor de props van de button component
 type ButtonProps = {
   children: React.ReactNode;
-  variant?: "primary" | "secondary";
-  icon?: keyof typeof icons;
+  variant?: "primary" | "secondary"; // Voor het kiezen van de styling van de knop
+  icon?: keyof typeof icons; // Voor het kiezen van de juiste icon
 };
 
-export function Button({ children, variant = "secondary", icon }: ButtonProps) {
-  const Icon = icon ? icons[icon] : null;
+// Verschillende variaties voor primaire en secundair knop
+const variantStyles = {
+  primary: "bg-[var(--primary-bg-color)] text-[var(--primary-text-color)] border-1 border-[var(--primary-text-color)] hover:bg-[var(--primary-text-color)] hover:text-[var(--secondary-text-color)]",
+  secondary: "bg-[var(--secondary-bg-color)] text-[var(--primary-text-color)] hover:bg-[var(--primary-text-color)] hover:text-[var(--secondary-text-color)]",
+};
 
-  const variantStyles = {
-    primary: "bg-[var(--primary-bg-color)] text-[var(--primary-text-color)] border-1 border-[var(--primary-text-color)] hover:bg-[var(--primary-text-color)] hover:text-[var(--secondary-text-color)]",
-    secondary: "bg-[var(--secondary-bg-color)] text-[var(--primary-text-color)] hover:bg-[var(--primary-text-color)] hover:text-[var(--secondary-text-color)]",
-  };
+// Button component
+export function Button({ children, variant = "secondary", icon }: ButtonProps) {
+
+  // Variabele die het juiste icon component selecteert. Als er geen icon prop is wordt er geen icon gerenderd
+  const Icon = icon ? icons[icon] : null;
 
   return (
     <button className={`flex items-center gap-3 rounded-[var(--border-radius-sm)] py-[7px] px-[10px] sm:py-[12px] sm:px-[32px] cursor-pointer transition-colors ${variantStyles[variant]}`}>
