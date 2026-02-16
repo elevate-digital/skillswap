14885588e681ff7e93845e3405380277b63a54a3
ambersr <amber.schalker@hva.nl>
2026-02-16 10:10:04 +0100

    feat: if statement toegevoegd voor button met alleen een icon #8

--- apps/web/lib/components/atoms/Button.tsx
+++ apps/web/lib/components/atoms/Button.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 // Type definitie voor de props van de button component
 type ButtonProps = {
-  children: React.ReactNode;
+  children?: React.ReactNode;
   variant?: "primary" | "secondary"; // Voor het kiezen van de styling van de knop
   icon?: React.ElementType; // Voor het kiezen van de juiste icon
 };
@@ -17,9 +17,8 @@ const variantStyles = {
 export function Button({ children, variant = "secondary", icon: Icon, }: ButtonProps) {
 
   return (
-    <button className={`flex items-center gap-3 rounded-[var(--border-radius-sm)] py-[7px] px-[10px] sm:py-[12px] sm:px-[32px] cursor-pointer transition-colors ${variantStyles[variant]}`}>
-    
-      {Icon && <Icon size={25} className="hidden sm:block" />}
+    <button className={`flex items-center justify-center gap-3 rounded-[var(--border-radius-sm)] cursor-pointer transition-colors ${variantStyles[variant]} ${!children && Icon ? "p-[12px] sm:p-[12px]" : "py-[7px] px-[10px] sm:py-[12px] sm:px-[32px]"}`}>
+      {Icon && (<Icon className={!children ? "block text-[22px] md:text-[20px]" : "hidden sm:block text-[24px] md:text-[22px]"} />)}
       {children}
     </button>
   );
