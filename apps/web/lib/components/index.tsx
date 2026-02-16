9d829967b1c40653ef3ff00e8963cd80a3c4bf17
ambersr <amber.schalker@hva.nl>
2026-02-16 10:58:30 +0100

    feat: container component ingeladen in de index.tsx #12

--- apps/web/lib/components/index.tsx
+++ apps/web/lib/components/index.tsx
@@ -2,6 +2,9 @@
 export { Button } from "./atoms/Button";
 
 // Molecules
+export { Header } from "./molecules/Header";
 
+// Organisms
+export { Container } from "./organisms/Container";
 
 // Images
