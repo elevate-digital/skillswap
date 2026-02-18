93295959b66b729d7e23ff3c47f0e8bd4df4ffaf
ambersr <amber.schalker@hva.nl>
2026-02-18 13:34:05 +0100

    feat: componenten ingeladen in index.tsx #14 #13

--- apps/web/lib/components/index.tsx
+++ apps/web/lib/components/index.tsx
@@ -1,10 +1,21 @@
 // Atoms
 export { Button } from "./atoms/Button";
+export { Tag } from "./atoms/Tag";
+export { IconBg } from "./atoms/IconBg";
+export { Icon } from "./atoms/Icon";
+export { ProfilePicture } from "./atoms/ProfilePicture";
 
 // Molecules
+export { DiscussionCard } from "./molecules/DiscussionCard";
 export { Header } from "./molecules/Header";
+export { Switch } from "./molecules/Switch";
+export { PopupHeader } from "./molecules/PopupHeader";
+export { PopupOverlay } from "./molecules/PopupOverlay";
 
 // Organisms
 export { Container } from "./organisms/Container";
+export { DiscussionCards } from "./organisms/DiscussionCards";
+export { PopupLogin } from "./organisms/PopupLogin";
+export { Form } from "./organisms/Form";
 
 // Images
