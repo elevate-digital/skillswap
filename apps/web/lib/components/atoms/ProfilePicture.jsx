07abd736365e826aaded3e981a81d9c68de81645
ambersr <amber.schalker@hva.nl>
2026-02-18 13:32:19 +0100

    feat: profile img als component gemaakt #14

---
+++ apps/web/lib/components/atoms/ProfilePicture.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export function ProfilePicture({ name }) {
+    return (
+      <p className="w-[45px] h-[45px] mr-[30px] rounded-full bg-[var(--primary-highlight-color)] flex items-center justify-center !text-[var(--secondary-text-color)] !font-[var(--font-weight-m)]">EV</p>
+    );
+  };
+  
+  export default ProfilePicture;
+  
