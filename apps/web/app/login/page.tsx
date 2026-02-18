3a827a1b6bbafff426bb767d83a6e7fdffb8e7a4
ambersr <amber.schalker@hva.nl>
2026-02-18 14:38:33 +0100

    fix: react focus lock geïmporteerd #13

--- apps/web/app/login/page.tsx
+++ apps/web/app/login/page.tsx
@@ -1,12 +1,17 @@
+"use client";
+
+import FocusLock from "react-focus-lock";
 import { PopupLogin } from "@/lib/components";
 import { PopupOverlay } from "@/lib/components";
 
 export default function Home() {
   return ( 
     <main>
-        <PopupOverlay>
-                <PopupLogin title="Welkom bij SkillSwap" description="Log in of maak een account aan om skills te delen en hulp te vragen."/>
-        </PopupOverlay>
+        <FocusLock>
+          <PopupOverlay>
+                  <PopupLogin title="Welkom bij SkillSwap" description="Log in of maak een account aan om skills te delen en hulp te vragen."/>
+          </PopupOverlay>
+        </FocusLock>
     </main>
   )
 }
--- apps/web/lib/components/molecules/Switch.tsx
+++ apps/web/lib/components/molecules/Switch.tsx
@@ -5,8 +5,8 @@ import { Form } from "@/lib/components";
 export function Switch() {
     return (
     
-    <div className="flex flex-col gap-3">
-        <input type="checkbox" id="toggle" className="peer hidden" />
+    <div className="relative flex flex-col gap-3">
+        <input type="checkbox" id="toggle" className="peer w-[100%] h-[2.5em] absolute appearance-none" />
 
         <div className="relative flex bg-[#E5E7E2] p-1 rounded-full w-full before:absolute before:top-1 before:left-1 before:h-[calc(100%-8px)] before:w-[calc(50%-4px)] before:bg-[var(--third-bg-color)] before:rounded-full before:transition-transform before:duration-300 peer-checked:before:translate-x-full">
             <label htmlFor="toggle" className="flex-1 text-center py-1 cursor-pointer relative z-10 text-[var(--primary-text-color)] peer-checked:text-[var(--primary-text-color)] transition">
