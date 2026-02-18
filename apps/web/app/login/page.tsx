50da32be4597d8413f448199fbefe977eb1fc149
ambersr <amber.schalker@hva.nl>
2026-02-18 13:25:02 +0100

    feat: route login aangemaakt #13

---
+++ apps/web/app/login/page.tsx
@@ -0,0 +1,12 @@
+import { PopupLogin } from "@/lib/components";
+import { PopupOverlay } from "@/lib/components";
+
+export default function Home() {
+  return ( 
+    <main>
+        <PopupOverlay>
+                <PopupLogin title="Welkom bij SkillSwap" description="Log in of maak een account aan om skills te delen en hulp te vragen."/>
+        </PopupOverlay>
+    </main>
+  )
+}
