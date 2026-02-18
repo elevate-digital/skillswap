fecac61a70c54336620a1f87e4c3f8433d6a81f1
ambersr <amber.schalker@hva.nl>
2026-02-18 13:53:56 +0100

    fix: test deployment

---
+++ apps/web/lib/components/atoms/Icon.tsx
@@ -0,0 +1,10 @@
+"use client";
+
+import React from 'react';
+import { StarIcon } from "@phosphor-icons/react";
+
+export function Icon() {
+    return (
+        <StarIcon weight="regular" width={45} height={45} />
+    )
+}
