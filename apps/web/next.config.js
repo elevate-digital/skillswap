4aff6cb2cc94abe0c2a2ef3a4794ec9cf3932e02
ambersr <amber.schalker@hva.nl>
2026-02-25 15:45:39 +0100

    fix: api route gedefiniëerd in config.js #15

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,4 +1,17 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async rewrites() {
+    return [
+      {
+        source: "/api/register",
+        destination: "https://skillswap-api-yw06.onrender.com/user/register",
+      },
+      {
+        source: "/api/login",
+        destination: "https://skillswap-api-yw06.onrender.com/user/login",
+      },
+    ];
+  },
+};
 
 export default nextConfig;
