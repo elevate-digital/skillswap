5d88f638ea676c792131e48e35ccb5c73ba315c3
ambersr <amber.schalker@hva.nl>
2026-03-02 14:19:31 +0100

    fix: login en register form status gedefiniëerd #15

--- apps/web/lib/components/organisms/LoginForm.tsx
+++ apps/web/lib/components/organisms/LoginForm.tsx
@@ -9,11 +9,10 @@ import { useAuth } from "@/lib/components";
 export function LoginForm() {
   const { user, login, logout } = useAuth();
 
-  const [form, setForm] = useState({ email: "", password: "" });
-  const [loading, setLoading] = useState(false);
+  const [form, setForm] = useState({ email: "", password: "" }); 
+  const [status, setStatus] = useState<"idle" | "loading" | "success" | "error">("idle"); 
   const [error, setError] = useState<string | null>(null);
-  const [success, setSuccess] = useState(false);
-
+  
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const { name, value } = e.target;
     setForm(prev => ({ ...prev, [name]: value }));
@@ -21,7 +20,7 @@ export function LoginForm() {
 
   const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
-    setLoading(true);
+    setStatus("loading");
     setError(null);
 
     try {
@@ -34,17 +33,15 @@ export function LoginForm() {
 
       // user opslaan via context
       login(response.data.user);
-
-      setSuccess(true);
+      setStatus("success");
     } catch (error) {
       console.error("Login failed:", error);
       setError("Onjuiste email of wachtwoord");
-    } finally {
-      setLoading(false);
-    }
-  };
+      setStatus("error");
+  } 
+};
 
-  if (loading) {
+  if (status === "loading") {
     return (
       <section className="flex flex-col gap-5 items-center bg-[#fff] px-[2em] py-[3em] rounded-[12px]">
         <h2 className="text-blue-600 animate-pulse !text-[20px]">Bezig met inloggen…</h2>
@@ -53,7 +50,7 @@ export function LoginForm() {
     );
   }
 
-  if (user && success) {
+  if (status === "success" && user) {
     return (
       <>
         <section className="flex flex-col gap-5 items-center bg-[#fff] px-[2em] py-[3em] rounded-[12px]">
@@ -80,7 +77,7 @@ export function LoginForm() {
     );
   }
 
-  if (user && !success) {
+  if (user && status !== "success") {
     return (
       <>
         <section className="flex flex-col gap-5 items-center bg-[#fff] px-[2em] py-[3em] rounded-[12px]">
@@ -109,27 +106,13 @@ export function LoginForm() {
 
   return (
     <form onSubmit={handleSubmit} className="flex flex-col gap-3 login-form">
-      <EmailField
-        name="email"
-        label="Email"
-        value={form.email}
-        onChange={handleChange}
-        placeholder="Bijv. janjansen@gmail.com"
-      />
+      <EmailField name="email" label="Email" value={form.email} onChange={handleChange} placeholder="Bijv. janjansen@gmail.com" />
 
-      <PasswordField
-        name="password"
-        label="Password"
-        value={form.password}
-        onChange={handleChange}
-        placeholder="Hier jouw wachtwoord"
-      />
+      <PasswordField name="password" label="Password" value={form.password} onChange={handleChange} placeholder="Hier jouw wachtwoord" />
 
       {error && <p className="text-red-600">{error}</p>}
 
-      <Button type="submit" variant="secondary" icon={UserPlusIcon}>
-        Inloggen
-      </Button>
+      <Button type="submit" variant="secondary" icon={UserPlusIcon}>Inloggen</Button>
 
       <div className="flex items-center gap-4 my-2 w-full">
         <div className="flex-1 h-px bg-gray-300" />
@@ -137,10 +120,7 @@ export function LoginForm() {
         <div className="flex-1 h-px bg-gray-300" />
       </div>
 
-      <a
-        className="bg-[var(--primary-bg-color)] text-[var(--primary-text-color)] border-1 border-[var(--primary-text-color)] hover:bg-[var(--primary-text-color)] hover:text-[var(--secondary-text-color)] flex items-center justify-center gap-3 rounded-[var(--border-radius-sm)] cursor-pointer transition-colors sm:p-[12px] lg:p-[12px] py-[7px] px-[10px] lg:py-[12px] lg:px-[32px]"
-        href="/"
-      >
+      <a className="bg-[var(--primary-bg-color)] text-[var(--primary-text-color)] border-1 border-[var(--primary-text-color)] hover:bg-[var(--primary-text-color)] hover:text-[var(--secondary-text-color)] flex items-center justify-center gap-3 rounded-[var(--border-radius-sm)] cursor-pointer transition-colors sm:p-[12px] lg:p-[12px] py-[7px] px-[10px] lg:py-[12px] lg:px-[32px] "href="/" >
         <UserIcon height={20} width={20} />
         Ga naar dashboard
       </a>
--- apps/web/lib/components/organisms/RegisterForm.tsx
+++ apps/web/lib/components/organisms/RegisterForm.tsx
@@ -7,116 +7,100 @@ import { UserPlusIcon, UserIcon } from "@phosphor-icons/react";
 import { NameField } from "@/lib/components";
 import { EmailField } from "@/lib/components";
 import { PasswordField } from "@/lib/components";
+import { useAuth } from "@/lib/components"
 
 export function RegisterForm() {
+  const { user, login, logout } = useAuth();
+
   const [form, setForm] = useState({ name: "", email: "", password: "" });
-  const [user, setUser] = useState<any>(null);
-  const [loading, setLoading] = useState(false);
+  const [status, setStatus] = useState<"idle" | "loading" | "success" | "error">("idle"); 
   const [error, setError] = useState<string | null>(null);
-  const [success, setSuccess] = useState(false);
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const { name, value } = e.target;
     setForm(prev => ({ ...prev, [name]: value}))
   }
 
-  const handleLogout = () => {
-    setUser(null);
-    localStorage.removeItem("user");
-    window.dispatchEvent(new Event("auth-changed"));
-    setSuccess(false);
-  };
+  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
 
-  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-
-    setLoading(true);
+    setStatus("loading"); 
     setError(null);
       
-    console.log("Form submitted:", form); 
-
     try {
       const response = await axios.post("/api/register", form, { 
         headers: { "Content-Type": "application/json" } }
     );
-      console.log(response.data);
 
-      // Verwacht dat backend user terugstuurt (zelfde als login)
-      setUser(response.data.user);
+      login(response.data.user);
+      setStatus("success");
 
-      localStorage.setItem("user", JSON.stringify(response.data.user));
-      window.dispatchEvent(new Event("auth-changed"));
-
-      setSuccess(true);
-
-    } catch (error: any) {
-      console.error("Login failed:", error);
+    } catch (error) {
+      console.error("Register failed:", error);
       setError("Registreren mislukt. Probeer opnieuw.");  
-    } finally {
-      setLoading(false);
+      setStatus("error");
     }
   }
 
-  return (
-    <form onSubmit={handleSubmit} className="flex flex-col gap-3">
+  if (status === "loading") {
+    return (
+      <section className="flex flex-col gap-5 items-center bg-[#fff] px-[2em] py-[3em] rounded-[12px]">
+        <h2 className="text-blue-600 animate-pulse !text-[20px]">Account aanmaken…</h2>
+        <img className="animate-spin" src="/assets/loading-icon.svg" width={50} height={50}/>
+      </section>
+    ); 
+  }
 
-      {loading && (
+  if (status === "success" && user) {
+    return (
+      <>
         <section className="flex flex-col gap-5 items-center bg-[#fff] px-[2em] py-[3em] rounded-[12px]">
-          <h2 className="text-blue-600 animate-pulse !text-[20px]">Account aanmaken…</h2>
-          <img className="animate-spin" src="/assets/loading-icon.svg" width={50} height={50}/>
+          <h2 className="text-green-600 !text-[20px]">Account succesvol aangemaakt!</h2>
+          <p className="text-green-600">Ingelogd als {user.email}</p>
+
+          <a className="bg-[var(--secondary-bg-color)] text-[var(--primary-text-color)] hover:bg-[var(--primary-text-color)] hover:text-[var(--secondary-text-color)] flex items-center justify-center gap-3 rounded-[var(--border-radius-sm)] transition-colors lg:py-[12px] lg:px-[32px]" href="/">
+            <UserIcon /> Ga naar dashboard
+          </a>
         </section>
-      )}
 
-      {success && user && (
-        <>
-          <section className="flex flex-col gap-5 items-center bg-[#fff] px-[2em] py-[3em] rounded-[12px]">
-            <h2 className="text-green-600 !text-[20px]">Account succesvol aangemaakt!</h2>
-            <p className="text-green-600">Ingelogd als {user.email}</p>
+        <Button type="button" variant="secondary" onClick={logout}>
+          Uitloggen
+        </Button>
+      </>
+    )
+  }
 
-            <a className="bg-[var(--secondary-bg-color)] text-[var(--primary-text-color)] hover:bg-[var(--primary-text-color)] hover:text-[var(--secondary-text-color)] flex items-center justify-center gap-3 rounded-[var(--border-radius-sm)] transition-colors lg:py-[12px] lg:px-[32px]" href="/">
-              <UserIcon /> Ga naar dashboard
-            </a>
-          </section>
+  if (user && status === "idle") {
+    return (
+    <>
+      <section className="flex flex-col gap-5 items-center bg-[#fff] px-[2em] py-[3em] rounded-[12px]">
+        <h2 className="text-green-600 !text-[20px]">Je bent al ingelogd</h2>
+        <p className="text-green-600">{user.email}</p>
 
-          <Button type="button" variant="secondary" onClick={handleLogout}>
-            Uitloggen
-          </Button>
-        </>
-      )}
+        <a
+          className="bg-[var(--secondary-bg-color)] text-[var(--primary-text-color)] flex items-center justify-center gap-3 rounded-[var(--border-radius-sm)] transition-colors lg:py-[12px] lg:px-[32px]"
+          href="/"
+        >
+          <UserIcon /> Naar dashboard
+        </a>
+      </section>
 
-      {!user && !success && !loading && (
-        <>
-          <NameField name="name" label="Naam"value={form.name} onChange={handleChange} placeholder="Jan Jansen" />
-          <EmailField name="email" label="Email" value={form.email} onChange={handleChange} placeholder="Bijv. janjansen@gmail.com" />
-          <PasswordField name="password" label="Wachtwoord" value={form.password} onChange={handleChange} placeholder="Vul wachtwoord in" />
+      <Button type="button" variant="primary" onClick={logout}>
+        Uitloggen
+      </Button>
+    </>
+    )
+  }
 
-          {error && <p className="text-red-600">{error}</p>}
+  return (
+    <form onSubmit={handleSubmit} className="flex flex-col gap-3 register-form">
+      <NameField name="name" label="Naam"value={form.name} onChange={handleChange} placeholder="Jan Jansen" />
+      <EmailField name="email" label="Email" value={form.email} onChange={handleChange} placeholder="Bijv. janjansen@gmail.com" />
+      <PasswordField name="password" label="Wachtwoord" value={form.password} onChange={handleChange} placeholder="Vul wachtwoord in" />
 
-          <Button type="submit" variant="secondary" icon={UserPlusIcon}>Account aanmaken</Button>
-        </>
-      )}
+      {error && <p className="text-red-600">{error}</p>}
 
-      {/* ALREADY LOGGED IN */}
-      {!success && user && (
-        <>
-          <section className="flex flex-col gap-5 items-center bg-[#fff] px-[2em] py-[3em] rounded-[12px]">
-            <h2 className="text-green-600 !text-[20px]">Je bent al ingelogd</h2>
-            <p className="text-green-600">{user.email}</p>
-
-            <a
-              className="bg-[var(--secondary-bg-color)] text-[var(--primary-text-color)] flex items-center justify-center gap-3 rounded-[var(--border-radius-sm)] transition-colors lg:py-[12px] lg:px-[32px]"
-              href="/"
-            >
-              <UserIcon /> Naar dashboard
-            </a>
-          </section>
-
-          <Button type="button" variant="primary" onClick={handleLogout}>
-            Uitloggen
-          </Button>
-        </>
-      )}
-
+      <Button type="submit" variant="secondary" icon={UserPlusIcon}>Account aanmaken</Button>
     </form>
   );
 }
