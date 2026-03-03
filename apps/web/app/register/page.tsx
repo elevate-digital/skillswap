f519ebc97e929a5ff38c27e04bf29a5528a84940
ambersr <amber.schalker@hva.nl>
2026-03-03 10:40:42 +0100

    fix: register form geupdate met nieuwste versie #15

--- apps/web/app/register/page.tsx
+++ apps/web/app/register/page.tsx
@@ -1,21 +1,27 @@
-"use client";
+'use client';
 
 import { useSearchParams } from "next/navigation";
 import { AuthPopup } from "@/lib/components";
 import { PopupOverlay } from "@/lib/components";
-import { Form } from "@/lib/components";
+import { LoginForm } from "@/lib/components";
+import { RegisterForm } from "@/lib/components";
+import { DiscussionCard } from "@/lib/components";
 
-export default function Register() {
+export default function Login() {
+
   const params = useSearchParams();
   const mode = params.get("mode") === "register" ? "register" : "login";
 
   return ( 
-    <main id="inhoud">
+    <main id="inhoud" className="py-[1em] md:py-[2em]">
           <PopupOverlay>
-                  <AuthPopup title="Welkom bij SkillSwap" description="Log in of maak een account aan om skills te delen en hulp te vragen.">  
-                    <Form type={mode} />
-                  </AuthPopup>
+            <AuthPopup title="Welkom bij SkillSwap" description="Log in of maak een account aan om skills te delen en hulp te vragen.">  
+                {mode === "login" && <LoginForm />}
+                {mode === "register" && <RegisterForm />}
+            </AuthPopup>
           </PopupOverlay>
+
+          <DiscussionCard />
     </main>
   )
 }
