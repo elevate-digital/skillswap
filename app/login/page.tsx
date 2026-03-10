7167ea93d0485318e0925c1a5c08ec64e0676128
ambersr <amber.schalker@hva.nl>
2026-03-10 14:54:22 +0100

    fix: error render

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,5 +1,7 @@
 'use client';
 
+export const dynamic = "force-dynamic";
+
 import { useSearchParams } from "next/navigation";
 import { AuthPopup, PopupOverlay, LoginForm, RegisterForm, DiscussionCard } from "@/lib/components";
 
