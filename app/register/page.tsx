d8af625557d6913bfc0a314c4b17cd1ba580740e
ambersr <amber.schalker@hva.nl>
2026-03-10 14:56:01 +0100

    fix: render fix error #20

--- app/register/page.tsx
+++ app/register/page.tsx
@@ -1,5 +1,7 @@
 'use client';
 
+export const dynamic = "force-dynamic";
+
 import { useSearchParams } from "next/navigation";
 import { RegisterForm, DiscussionCard, LoginForm, PopupOverlay, AuthPopup } from "@/lib/components";
 
