5883f044c98475774b268bcbde447cc62be6c82c
ambersr <amber.schalker@hva.nl>
2026-03-10 14:04:17 +0100

    fix: error fix no js #20

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -10,7 +10,7 @@ export const metadata: Metadata = {
 
 export default function RootLayout({ children, }: Readonly<{ children: React.ReactNode; }>) {
   return (
-    <html lang="en" className="no-js">
+    <html lang="en" className="js">
       <head>
         <script
           dangerouslySetInnerHTML={{
