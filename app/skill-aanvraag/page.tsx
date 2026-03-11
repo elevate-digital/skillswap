4c84d4f80b623601d586f2efa012b87d87398661
ambersr <amber.schalker@hva.nl>
2026-03-11 14:55:03 +0100

    feat: juiste form geïmporteerd #21

--- app/skill-aanvraag/page.tsx
+++ app/skill-aanvraag/page.tsx
@@ -1,14 +1,13 @@
 'use client';
 
-import { AuthPopup, PopupOverlay, SkillForm, DiscussionCard } from "@/lib/components";
+import { AuthPopup, PopupOverlay, OfferForm, DiscussionCard } from "@/lib/components";
 
 export default function Skill() {
-
   return ( 
     <main id="inhoud" className="py-[1em] md:py-[2em] flex flex-col gap-5">
           <PopupOverlay>
             <AuthPopup title="Biedt je skill aan" description="Deel je kennis met andere studenten en junior developers." showHeader={true} showSwitch={false}>  
-                <SkillForm />
+                <OfferForm />
             </AuthPopup>
           </PopupOverlay>
 
