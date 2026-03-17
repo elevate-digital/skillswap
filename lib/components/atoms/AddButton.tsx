772f7aed60be4458951c2da9dd33c219ff0287a5
ambersr <amber.schalker@hva.nl>
2026-03-17 15:54:12 +0100

    fix: addbutton mobiel width en height meegegeven #19

--- lib/components/atoms/AddButton.tsx
+++ lib/components/atoms/AddButton.tsx
@@ -1,13 +1,12 @@
 'use client'
 
-import React from "react";
 import { LinkButton } from "@/lib/components";
 import { PlusCircleIcon } from "@phosphor-icons/react";
 
 export function AddButton() { 
   return (
     <div className="fixed bottom-[1.5em] right-[5%] md:hidden">
-      <LinkButton variant="primary" icon={PlusCircleIcon}><span className="sr-only">Skill of vraag aanmaken</span></LinkButton>
+      <LinkButton variant="primary" icon={PlusCircleIcon} className="w-[3.2em] h-[3.2em] !p-[1em]"><span className="sr-only">Skill of vraag aanmaken</span></LinkButton>
     </div>
  )
 }
